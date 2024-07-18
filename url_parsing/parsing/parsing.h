2fa30531d876b856cb08a937912a6fa7acb4e4bd
CuriosityKite <khaishat16@gmail.com>
2024-07-18 19:06:21 +0300

    url parsing

---
+++ url_parsing/parsing/parsing.h
@@ -0,0 +1,72 @@
+﻿#pragma once
+/*
+Необходимо написать парсер URL строки с использованием регулярных выражении regex.
+Структура URL:
+    <схема>:[//[<логин>[:<пароль>]@]<хост>[:<порт>]][/<URL‐путь>][?<параметры>][#<якорь>]
+
+*/
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <regex>
+
+class URL
+{
+    private:
+        std::string scheme;
+        std::string username;
+        std::string password;
+        std::string ost;
+        std::string port;
+        std::string path;
+        std::string query;
+        std::string fragment;
+    public:
+        URL(const std::string& sch, const std::string& usname, const std::string& passw, const std::string& os,
+            const std::string& prt, const std::string& pth, const std::string& quer, const std::string& frag)
+            : scheme(sch), username(usname), password(passw), ost(os), port(prt), path(pth), query(quer), fragment(frag) {}
+
+        void print() const
+        {
+            std::cout << "scheme: " << scheme
+                << " username: " << username
+                << " password: " << password
+                << " ost: " << ost
+                << " port: " << port
+                << " path: " << path
+                << " query: " << query
+                << " fragment: " << fragment << std::endl;
+        }
+
+        bool operator==(const URL& other) const
+        {
+            return scheme == other.scheme &&
+                username == other.username &&
+                password == other.password &&
+                ost == other.ost &&
+                port == other.port &&
+                path == other.path &&
+                query == other.query &&
+                fragment == other.fragment;
+        }
+
+        static bool compareURLs(const URL url1, const URL url2)
+        {
+            return url1 == url2;
+        }
+
+        static URL parsing(const std::string& urlString)
+        {
+            std::regex log_regex(R"~(^([a-z]+):\/\/(?:([a-z0-9.-]+):([a-z0-9.-]+)@)?([a-z0-9.-]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$)~");
+            std::smatch match;
+            if (std::regex_search(urlString, match, log_regex))
+            {
+                URL entry(match[1], match[2], match[3], match[4], match[5], match[6], match[7], match[8]);
+                return entry;
+            }
+            else
+                return URL("", "", "", "", "", "", "", "");
+        }
+};
---
+++ url_parsing/uni_test/uni_test.cpp
@@ -0,0 +1,93 @@
+﻿#define BOOST_TEST_MODULE MyTest
+#include "../parsing/parsing.h"
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(test_parsing_simple_url)
+{
+    std::string urlString = "https://ru.m.wikipedia.org/wiki/URL";
+    URL expectedURL("https", "", "", "ru.m.wikipedia.org", "", "wiki/URL", "", "");
+    URL actualURL = URL::parsing(urlString);
+
+    BOOST_CHECK(URL::compareURLs(expectedURL, actualURL));
+}
+
+BOOST_AUTO_TEST_CASE(test_parsing_with_credentials)
+{
+    std::string urlString = "http://user:password@example.com:8080/path?query#fragment";
+    URL expectedURL("http", "user", "password", "example.com", "8080", "path", "query", "fragment");
+    URL actualURL = URL::parsing(urlString);
+
+    BOOST_CHECK(URL::compareURLs(expectedURL, actualURL));
+}
+
+BOOST_AUTO_TEST_CASE(test_parsing_with_port)
+{
+    std::string urlString = "https://www.example.com:443/path";
+    URL expectedURL("https", "", "", "www.example.com", "443", "path", "", "");
+    URL actualURL = URL::parsing(urlString);
+
+    BOOST_CHECK(URL::compareURLs(expectedURL, actualURL));
+}
+
+BOOST_AUTO_TEST_CASE(test_parsing_with_query)
+{
+    std::string urlString = "https://www.example.com/path?param1=value1&param2=value33";
+    URL expectedURL("https", "", "", "www.example.com", "", "path", "param1=value1&param2=value33", "");
+    URL actualURL = URL::parsing(urlString);
+
+    BOOST_CHECK(URL::compareURLs(expectedURL, actualURL));
+}
+
+BOOST_AUTO_TEST_CASE(test_parsing_with_fragment)
+{
+    std::string urlString = "https://www.example.com/path#fragment";
+    URL expectedURL("https", "", "", "www.example.com", "", "path", "", "fragment");
+    URL actualURL = URL::parsing(urlString);
+
+    BOOST_CHECK(URL::compareURLs(expectedURL, actualURL));
+}
+
+BOOST_AUTO_TEST_CASE(test_parsing_with_empty_path)
+{
+    std::string urlString = "https://www.example.com/";
+    URL expectedURL("https", "", "", "www.example.com", "", "", "", "");
+    URL actualURL = URL::parsing(urlString);
+
+    BOOST_CHECK(URL::compareURLs(expectedURL, actualURL));
+}
+
+BOOST_AUTO_TEST_CASE(test_parsing_with_empty_query)
+{
+    std::string urlString = "https://www.example.com/path?";
+    URL expectedURL("https", "", "", "www.example.com", "", "path", "", "");
+    URL actualURL = URL::parsing(urlString);
+
+    BOOST_CHECK(URL::compareURLs(expectedURL, actualURL));
+}
+
+BOOST_AUTO_TEST_CASE(test_parsing_with_empty_fragment)
+{
+    std::string urlString = "https://www.example.com/path#";
+    URL expectedURL("https", "", "", "www.example.com", "", "path", "", "");
+    URL actualURL = URL::parsing(urlString);
+
+    BOOST_CHECK(URL::compareURLs(expectedURL, actualURL));
+}
+
+BOOST_AUTO_TEST_CASE(test_parsing_with_multiple_slashes)
+{
+    std::string urlString = "https://www.example.com/path";
+    URL expectedURL("https", "", "", "www.example.com", "", "path", "", "");
+    URL actualURL = URL::parsing(urlString);
+
+    BOOST_CHECK(URL::compareURLs(expectedURL, actualURL));
+}
+
+BOOST_AUTO_TEST_CASE(test_parsing_with_special_characters)
+{
+    std::string urlString = "https://www.example.com/path?param1=value1%20&param2=value2%23";
+    URL expectedURL("https", "", "", "www.example.com", "", "path", "param1=value1%20&param2=value2%23", "");
+    URL actualURL = URL::parsing(urlString);
+
+    BOOST_CHECK(URL::compareURLs(expectedURL, actualURL));
+}
